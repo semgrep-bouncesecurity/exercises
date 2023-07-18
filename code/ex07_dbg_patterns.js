51b8e0ce3c06559fccede55506fae74d12372834
kamenskymic <105462842+kamenskymic@users.noreply.github.com>
2023-07-18 18:27:50 +0300

    Update and rename ex7_dbg_patterns.js to ex07_dbg_patterns.js

--- code/ex07_dbg_patterns.js
+++ code/ex07_dbg_patterns.js
@@ -3,24 +3,24 @@ const el = element.innerHTML;
 class UnsafeFunctions {
 
     bad1(userInput) {
-    // ruleid: ex7_dbg_patterns
+    // ruleid: ex07_dbg_patterns
       el.innerHTML = '<div>' + userInput + '</div>';
     }
 
     bad2(userInput) {
       const name = '<div>' + userInput + '</div>';
-    // ruleid: ex7_dbg_patterns
+    // ruleid: ex07_dbg_patterns
       document.write(name);
     }
 
     ok1() {
       const name = "<div>it's ok</div>";
-    // ok: ex7_dbg_patterns
+    // ok: ex07_dbg_patterns
       el.innerHTML = name;
     }
 
     ok2() {
-    // ok: ex7_dbg_patterns
+    // ok: ex07_dbg_patterns
       document.write("<div>it's ok</div>");
     }
 
@@ -31,13 +31,13 @@ class SafeFunctions {
   // These functions look unsafe but are actually safe
 
     bad1(userInput) {
-    // ok: ex7_dbg_patterns
+    // ok: ex07_dbg_patterns
       el.innerHTML = '<div>' + userInput + '</div>';
     }
 
     bad2(userInput) {
       const name = '<div>' + userInput + '</div>';
-    // ok: ex7_dbg_patterns
+    // ok: ex07_dbg_patterns
       document.write(name);
     }
 }
